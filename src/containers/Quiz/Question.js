87d58169f9bdc710bc174b9ca54b1f57623a1bd1
Abhishek Singh <abhishek@tekhne.nl>
2017-01-15 17:01:02 +0530

    quiz logic

--- src/containers/Quiz/Question.js
+++ src/containers/Quiz/Question.js
@@ -1,35 +1,70 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { StyleSheet, css } from 'aphrodite';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+import Divider from 'material-ui/Divider';
+
+import {getWidth, getHeight} from '../../utils/config'
 
 class Question extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        answered: false,
+        answeredIndex: -1
+      };
+  };
+
+  _selectAnswer = (index) => {
+     console.log("Selected answer ",index);
+     if(index == this.props.question.answer) {
+       console.log("Correct answeer");
+     }else {
+       console.log("wrong answer");
+     }
+     this.setState({answered:true, answeredIndex: index});
+     this.props.onAnswered();
+  };
+
   render() {
     const {question} = this.props;
-    console.log(question);
     const questionText = question ? question.question : "not loaded";
     return (
         <div className={css(styles.container)}>
           <span className={css(styles.question)}>{questionText}</span>
-            <Table className={css(styles.options)}>
-                <TableBody>
-                    <TableRow>
-                    <TableRowColumn>{question.options[0].text}</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                    <TableRowColumn>{question.options[1].text}</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                    <TableRowColumn>{question.options[2].text}</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                    <TableRowColumn>{question.options[3].text}</TableRowColumn>
-                    </TableRow>
-                </TableBody>
-            </Table>
+          <img src={question.imageURL} style={imageStyle} />
+          <div className={css(styles.optionContainer)}>
+              { 
+                question.options.map((option, index) => this._renderOption(option, index))
+              }
+          </div>
+          <Divider />
+           {this.state.answered && <span>{question.explanation}</span>}
         </div>
     );
-  }
+  };
+
+  _renderOption = (option,index) => {
+      const correctAnswer = (index == this.props.question.answer && this.state.answered) ? true : false;
+      const wrongAnswer = (this.state.answered && !correctAnswer && index == this.state.answeredIndex) ? true : false;
+      return (
+         <RaisedButton label={option.text} style={optionStyle} primary={correctAnswer} 
+         secondary={wrongAnswer} onClick={()=>this._selectAnswer(index)} />
+      );
+  };
+}
+const optionStyle = {
+      display: 'flex',
+      flex:1,
+      flexDirection: 'row',
+      justyfyContent: 'center',
+      alignItems: 'center',
+      margin: 10
+}
+
+const imageStyle = {
+     width: getWidth(),
+     height: 'auto'
 }
 
 const styles = StyleSheet.create({
@@ -43,9 +78,12 @@ const styles = StyleSheet.create({
     question :{
       padding: 20,
     },
-    options: {
-      padding: 20,
-      maxWidth: 400,
+    optionContainer: {
+       display: 'flex',
+       flex: 1,
+       flexDirection: 'column',
+       alignItems: 'center',
+       justifyContent: 'space-between'
     }
 });
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { connect } from 'react-redux';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import {getDummyQuiz} from '../../redux/reducers/quiz'
 import Question from './Question';
@@ -8,24 +9,42 @@ import Question from './Question';
 class Quiz extends Component {
   constructor(props) {
       super(props);
+      this.state = {
+        questionIndex: 0,
+        answerd: false
+      };
       this.props.dispatch(getDummyQuiz());
   };
+
+  _nextQuestion = () => {
+      this.setState({questionIndex:this.state.questionIndex+1, answerd: false});
+  }
   
   _getQuestion = () => {
      if(this.props.quizlist) {
-         return this.props.quizlist[0];
+         return this.props.quizlist[this.state.questionIndex];
      }
      return undefined;
+  }
+
+  _answerdQuestion = () => {
+     this.setState({answerd:true})
   }
 
   render() {
     const selectedQuestion = this._getQuestion();
     return (
         <div className={css(styles.container)}>
-            {selectedQuestion && <Question question={selectedQuestion}/> }
+            {selectedQuestion && <Question question={selectedQuestion} onAnswered={this._answerdQuestion}/> }
+             <RaisedButton label="Next Question" disabled={!this.state.answerd} 
+             style={nextButtonStyle} onClick={this._nextQuestion} />
         </div>
     );
   }
+}
+
+const nextButtonStyle = {
+      margin: 10
 }
 
 const styles = StyleSheet.create({
